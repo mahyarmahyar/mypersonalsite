bdc570767cc0c3a35bf1bf362485d2d40b96fefe
mahyarmahyar <147530902+mahyarmahyar@users.noreply.github.com>
2024-05-22 15:22:06 +0330

    first action

---
+++ website/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
+from django.contrib import messages
+
+
+def index_view(request):
+    return render(request, 'website/index.html')
+
+
+def about_view(request):
+    return render(request, 'website/about.html')
+
+
+def elements_view(request):
+    return render(request, 'website/elements.html')
+
+
+def contact_view(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'We Received Your Massage')
+        else:
+            messages.error(request, 'An error occurred')
+    form = ContactForm()
+    return render(request, 'website/contact.html', {'form': form})
+
+
+def newsletter_view(request):
+    if request.method == 'POST':
+        form = NewsletterForm(request.POST)
+        if form. is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+    else:
+        return HttpResponseRedirect('/')
