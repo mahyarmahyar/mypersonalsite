e15225b8a26850b8198a9671b9394f67cf2a34e4
mahyarmahyar <147530902+mahyarmahyar@users.noreply.github.com>
2024-05-24 17:57:54 +0330

    blog

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+from blog.views import *
+from django.urls import path
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('', blog_view, name='index'),
+    path('single/', blog_single, name='single'),
+
+]
