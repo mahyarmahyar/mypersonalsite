552186defd2c5cb92d303829f372d457d13c8b10
mahyarmahyar <147530902+mahyarmahyar@users.noreply.github.com>
2024-06-07 17:17:23 +0330

    robots

--- personalsite/settings.py
+++ personalsite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.humanize',
     'accounts',
     'captcha',
+    'robots',
 
 
 ]
--- personalsite/urls.py
+++ personalsite/urls.py
@@ -36,6 +36,7 @@ urlpatterns = [
     path('captcha/', include('captcha.urls')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
          name='django.contrib.sitemaps.views.sitemap'),
+    path('robots.txt', include('robots.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
